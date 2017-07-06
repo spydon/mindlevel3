910b2a19380f85043e5b5c44dfab609bf45410cd
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-07-06 15:51:18 -0400

    Removed dummy model for lists

--- app/src/main/java/net/mindlevel/MissionsRecyclerViewAdapter.java
+++ app/src/main/java/net/mindlevel/MissionsRecyclerViewAdapter.java
@@ -1,6 +1,5 @@
 package net.mindlevel;
 
-import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,15 +7,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import net.mindlevel.MissionsFragment.OnListFragmentInteractionListener;
-import net.mindlevel.dummy.DummyContent.DummyItem;
 import net.mindlevel.model.Mission;
 
 import java.util.List;
 
 /**
- * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * {@link RecyclerView.Adapter} that can display a {@link Mission} and makes a call to the
  * specified {@link OnListFragmentInteractionListener}.
- * TODO: Replace the implementation with code for your data type.
  */
 public class MissionsRecyclerViewAdapter extends RecyclerView.Adapter<MissionsRecyclerViewAdapter.ViewHolder> {
 
