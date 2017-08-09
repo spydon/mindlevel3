4c0ca8cb5b7ab576f0ef6a84da4cdb6581508916
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-08-09 17:05:28 -0500

    Unmarshalling class for like

---
+++ app/src/main/java/net/mindlevel/model/Like.java
@@ -0,0 +1,11 @@
+package net.mindlevel.model;
+
+public class Like {
+    public final boolean first;
+    public final String score;
+
+    public Like(boolean first, String score) {
+        this.first = first;
+        this.score = score;
+    }
+}
