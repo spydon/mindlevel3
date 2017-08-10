26ca32838c732a1b21ab77e3a36b7ebda9a5584d
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-08-10 18:05:15 -0500

    Remove facebook dependency

--- app/src/main/java/net/mindlevel/AccomplishmentActivity.java
+++ app/src/main/java/net/mindlevel/AccomplishmentActivity.java
@@ -17,25 +17,15 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
-import com.facebook.share.model.SharePhoto;
-import com.facebook.share.model.SharePhotoContent;
-import com.facebook.share.widget.ShareButton;
 
 import net.mindlevel.model.Accomplishment;
 import net.mindlevel.util.ImageUtil;
-
-import static android.view.View.GONE;
 
 public class AccomplishmentActivity extends AppCompatActivity {
 
     private ImageLikeView imageView;
     private Activity activity;
     private ProgressBar progressBar;
-    private ShareButton facebookButton;
 
     private final int SHARE = 1;
 
@@ -65,18 +55,15 @@ public class AccomplishmentActivity extends AppCompatActivity {
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(facebookButton.isEnabled()) {
-                    //facebookButton.performClick();
-                    Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-                    sharingIntent.setType("image/*");
-                    sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, accomplishment.title);
-                    sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, accomplishment.description);
-                    Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
-                    String path = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "", null);
-                    Uri uri = Uri.parse(path);
-                    sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
-                    activity.startActivityForResult(Intent.createChooser(sharingIntent, "Share using"), SHARE);
-                }
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("image/*");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, accomplishment.title);
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, accomplishment.description);
+                Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                String path = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "", null);
+                Uri uri = Uri.parse(path);
+                sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                activity.startActivityForResult(Intent.createChooser(sharingIntent, "Share using"), SHARE);
             }
         });
 
@@ -88,13 +75,12 @@ public class AccomplishmentActivity extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progress);
         Glide.with(this)
                 .load(url)
-                .listener(shareLoading)
+                .listener(new ProgressBarController(progressBar))
                 .into(imageView);
 
         TextView titleView = (TextView) findViewById(R.id.title);
         TextView scoreView = (TextView) findViewById(R.id.score);
         TextView descriptionView = (TextView) findViewById(R.id.description);
-        facebookButton = (ShareButton) findViewById(R.id.fb_share_button);
         titleView.setText(accomplishment.title);
         scoreView.setText(Integer.toString(accomplishment.score));
         descriptionView.setText(accomplishment.description);
@@ -103,28 +89,4 @@ public class AccomplishmentActivity extends AppCompatActivity {
         imageView.buildDrawingCache();
 
     }
-
-    private RequestListener shareLoading = new RequestListener() {
-        @Override
-        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target target, boolean isFirstResource) {
-            progressBar.setVisibility(GONE);
-            return false;
-        }
-
-        @Override
-        public boolean onResourceReady(Object resource, Object model, Target target, DataSource dataSource, boolean isFirstResource) {
-            progressBar.setVisibility(GONE);
-            facebookButton.setEnabled(true);
-            Bitmap bitmap = ((BitmapDrawable) resource).getBitmap();
-            SharePhoto photo = new SharePhoto.Builder()
-                    .setBitmap(bitmap)
-                    .build();
-            SharePhotoContent content = new SharePhotoContent.Builder()
-                    .addPhoto(photo)
-                    .build();
-
-            facebookButton.setShareContent(content);
-            return false;
-        }
-    };
 }
