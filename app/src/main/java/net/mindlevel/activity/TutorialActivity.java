d0697c04214b5e573d8b538d246493c007b9dde5
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-10-18 22:16:43 -0500

    Tutorial for the icons

---
+++ app/src/main/java/net/mindlevel/activity/TutorialActivity.java
@@ -0,0 +1,27 @@
+package net.mindlevel.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import net.mindlevel.R;
+
+public class TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
